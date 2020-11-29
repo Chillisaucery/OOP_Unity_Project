21f4bf6141b4933bb801c9f5ca9d4219dbee01ec
TrNgTien <ngoctien27122001@gmail.com>
2020-11-29 20:23:56 +0700

    Create functionality
    - make the minion can go up and down
    - make the minion can go right and left
    - make minion auto attack
    - make minions are bigger and they have more health. May be can make the minion auto recover

--- OOP_Unity_Project/Assets/Scripts/Enemies AI/LargeSizeControl.cs
+++ OOP_Unity_Project/Assets/Scripts/Enemies AI/LargeSizeControl.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class LargeSizeControl : MonoBehaviour
 {
-   
+    private Transform transform;
+    private HealthControl healthControl;
+    public float sizeMultiplier = 2f;
+    public float healPerSecond = 1f;
+    public float healthMultiplier = 2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        healthControl = GetComponent<HealthControl>();
+        transform.localScale += new Vector3(sizeMultiplier, sizeMultiplier, 1);
+        healthControl.health *= healthMultiplier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthControl.TakeDamage(-1 * healPerSecond);
+    }
 }
