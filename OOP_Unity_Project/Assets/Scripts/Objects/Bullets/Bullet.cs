00c22d56814a60b4c088820183a1897d4c081563
Bill-Fashion <66458634+Bill-Fashion@users.noreply.github.com>
2020-12-07 17:28:35 +0700

    Complete Bullet object

--- OOP_Unity_Project/Assets/Scripts/Objects/Bullets/Bullet.cs
+++ OOP_Unity_Project/Assets/Scripts/Objects/Bullets/Bullet.cs
@@ -4,5 +4,48 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-  
+    public float speed = 20f;
+    public float damage = 10f;
+    public float force = 200f;
+    private Rigidbody2D rb;
+    public GameObject onDestroyEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //On trigger (currently only trigerred by the 2D collider
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+
+        HealthControl targetHealth = collision.GetComponent<HealthControl>();
+
+        //Animate the effect when being destroyed
+        Instantiate(onDestroyEffect, transform.position, transform.rotation);
+
+        //Deal damage to the target
+        if (targetHealth != null)
+        {
+            targetHealth.TakeDamage(damage);
+            if (!collision.GetComponent<Bullet>())
+                Destroy(gameObject);
+        }
+
+        //Make the target fall back
+        //Rigidbody2D targetBody = collision.GetComponent<Rigidbody2D>();
+        //targetBody.AddForce(new Vector2(force*transform.right.x, 0), ForceMode2D.Impulse);
+
+
+
+    }
 }
--- OOP_Unity_Project/Assets/Scripts/Objects/SelfDestruct.cs
+++ OOP_Unity_Project/Assets/Scripts/Objects/SelfDestruct.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class SelfDestruct : MonoBehaviour
 {
-    
+    public float timeLimit = 20f;
+    private float timer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timeLimit)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
