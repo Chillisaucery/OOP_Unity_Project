8b43e5380653c4321bc9a00dd6b3affaf5475985
TrNgTien <ngoctien27122001@gmail.com>
2021-01-02 11:11:50 +0700

    Added the countdown and the score feature.
    More enemies.
    There is a countdown now, it will destroy the player when time up.
    Enemies now yield score. Destroy them to earn score.

---
+++ OOP_Unity_Project/Assets/Scripts/Level/Countdown.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+
+    public GameObject textObject;
+    public GameObject player;
+    public int minutes = 1;
+    public int seconds = 30;
+    public int endgame = 0;
+    public float TIMERMAX=999f;
+
+    private float timer;
+    private Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = TIMERMAX;
+        text = textObject.GetComponent<Text>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+	    text.text = "Time left: " + minutes + " : " + seconds;
+
+    	timer -= Time.deltaTime;
+        if (timer <= 0) {
+	        seconds--;
+	        timer = TIMERMAX;
+	    }
+	    if (seconds <= 0) {
+	        seconds = 59;
+            minutes--;
+	    }
+        if (minutes <0)
+        {
+            Destroy(player);
+            Destroy(textObject);
+        }
+    }
+}
---
+++ OOP_Unity_Project/Assets/Scripts/Level/YieldScore.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YieldScore : MonoBehaviour
+{
+
+    public int score=100;
+
+    private DisplayScore displayScore;
+    private Countdown countdown;
+    private GameObject player;
+    private HealthControl health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        displayScore = FindObjectsOfType<DisplayScore>()[0];
+        countdown = FindObjectOfType<Countdown>();
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
+        health = player.GetComponent<HealthControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDestroy()
+    {
+        int healthValue = (int)health.health;
+        score *= healthValue / (50);
+
+        displayScore.score += score;
+    }
+}
